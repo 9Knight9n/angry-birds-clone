64508f152da706909528c655cdd0ad27aad84ea2
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-01-16 21:38:31 +0330

    added bird config

---
+++ Assets/Scripts/Configs/BirdConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "BirdConfig", menuName = "Config/BirdConfig")]
+public class BirdConfig : ScriptableObject
+{
+    public float mass;
+    public Sprite sprite;
+    public float scale;
+    public float gravity;
+}
---
+++ Assets/Scripts/bird/BirdController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public BirdConfig config;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<SpriteRenderer>().sprite = config.sprite;
+        GetComponent<Rigidbody2D>().mass = config.mass;
+        GetComponent<Transform>().localScale = new Vector3(config.scale, config.scale, config.scale);
+        GetComponent<Rigidbody2D>().gravityScale = config.gravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
