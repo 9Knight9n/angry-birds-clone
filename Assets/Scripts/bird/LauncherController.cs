fefe1d70169dd279a2d4d7c2230532a15aaa7dc4
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-01-17 21:59:29 +0330

    fixed bugs

--- Assets/Scripts/bird/LauncherController.cs
+++ Assets/Scripts/bird/LauncherController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LauncherController : BirdController
 {
+    private float _magnifier;
     // Start is called before the first frame update
     void Start()
     {
