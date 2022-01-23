ed1e1b8a3fbef62040b829513a54f98b9c38a2f8
abed ebadi <abdi.eb79@gmail.com>
2022-01-23 16:53:49 +0330

    Worked on menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject Panel;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void Level()
+    {
+        Panel.GetComponent<Animator>().SetTrigger("Pop");
+    }
+    
+    public void PlayGame()
+    {
+        Debug.Log("Clicked on play");
+    }
+}
